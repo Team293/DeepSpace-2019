97ba2514b50e85aa4c758f8456182914eff8381d
Jonah <jss10713@gmail.com>
2019-02-02 14:14:30 -0500

    Making the elevator work and fixing the shoot function

--- Drivetrain2019/src/main/java/org/usfirst/frc293/Drivetrain2019/commands/ElevatorDown.java
+++ Drivetrain2019/src/main/java/org/usfirst/frc293/Drivetrain2019/commands/ElevatorDown.java
@@ -43,6 +43,7 @@ public class ElevatorDown extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+        Robot.elevator.elevatorDown();
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -60,5 +61,6 @@ public class ElevatorDown extends Command {
     // subsystems is scheduled to run
     @Override
     protected void interrupted() {
+        Robot.elevator.elevatorStop();
     }
 }
